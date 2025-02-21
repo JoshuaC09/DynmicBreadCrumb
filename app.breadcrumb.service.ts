ae1161095f39d8f7de6bf6177b1e696bc6bc62ef
JoshuaC09 <117239417+JoshuaC09@users.noreply.github.com>
2025-02-21 11:33:09 +0800

    Update app.breadcrumb.service.ts

--- app.breadcrumb.service.ts
+++ app.breadcrumb.service.ts
@@ -83,3 +83,124 @@ export class BreadcrumbService {
     return data['breadcrumb'] || null;
   }
 }
+
+
+
+
+
+
+
+
+//for nested parameter
+
+// import { Injectable } from '@angular/core';
+// import { ActivatedRoute, NavigationEnd, Router, ActivatedRouteSnapshot } from '@angular/router';
+// import { BehaviorSubject } from 'rxjs';
+// import { filter } from 'rxjs/operators';
+
+// export interface Breadcrumb {
+//   label: string;
+//   url?: string;
+// }
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class BreadcrumbService {
+//   private readonly breadcrumbs$ = new BehaviorSubject<Breadcrumb[]>([]);
+
+//   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+//     this.router.events.pipe(
+//       filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+//     ).subscribe(() => {
+//       const breadcrumbs: Breadcrumb[] = [];
+
+//       const params = new Map<string, string>();
+//       let route: ActivatedRoute | null = this.activatedRoute;
+//       while (route) {
+//         ['clientCode', 'branchCode'].forEach(param => {
+//           if (route?.snapshot.paramMap.has(param)) {
+//             params.set(param, route.snapshot.paramMap.get(param)!);
+//           }
+//         });
+//         route = route.firstChild;
+//       }
+
+//       params.forEach(value => breadcrumbs.push({ label: value }));
+//       this.addBreadcrumb(this.router.routerState.snapshot.root, breadcrumbs);
+//       this.breadcrumbs$.next(breadcrumbs);
+//     });
+//   }
+
+//   private addBreadcrumb(route: ActivatedRouteSnapshot, breadcrumbs: Breadcrumb[]) {
+//     const label = route.data['breadcrumb'];
+//     if (label && (!route.parent?.data['breadcrumb'] || label !== route.parent.data['breadcrumb'])) {
+//       breadcrumbs.push({ label });
+//     }
+//     if (route.firstChild) {
+//       this.addBreadcrumb(route.firstChild, breadcrumbs);
+//     }
+//   }
+
+//   getBreadcrumbs() {
+//     return this.breadcrumbs$.asObservable();
+//   }
+// }
+
+
+// fastest direct 
+
+// import { Injectable } from '@angular/core';
+// import { ActivatedRoute, NavigationEnd, Router, ActivatedRouteSnapshot } from '@angular/router';
+// import { BehaviorSubject } from 'rxjs';
+// import { filter } from 'rxjs/operators';
+
+// export interface Breadcrumb {
+//   label: string;
+//   url?: string;
+// }
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class BreadcrumbService {
+//   private readonly breadcrumbs$ = new BehaviorSubject<Breadcrumb[]>([]);
+
+//   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+//     this.router.events.pipe(
+//       filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+//     ).subscribe(() => {
+//       const breadcrumbs: Breadcrumb[] = [];
+
+//       const params = this.activatedRoute.snapshot.root.firstChild?.params || {};
+//       if (params['clientCode']) { breadcrumbs.push({ label: params['clientCode'] });}
+//       if (params['branchCode']) { breadcrumbs.push({ label: params['branchCode'] });}
+
+//       this.addBreadcrumb(this.router.routerState.snapshot.root, breadcrumbs);
+//       this.breadcrumbs$.next(breadcrumbs);
+//     });
+//   }
+
+//   private addBreadcrumb(route: ActivatedRouteSnapshot, breadcrumbs: Breadcrumb[]) {
+//     const label = route.data['breadcrumb'];
+//     if (label && (!route.parent?.data['breadcrumb'] || label !== route.parent.data['breadcrumb'])) {
+//       breadcrumbs.push({ label });
+//     }
+//     if (route.firstChild) {
+//       this.addBreadcrumb(route.firstChild, breadcrumbs);
+//     }
+//   }
+
+//   getBreadcrumbs() {
+//     return this.breadcrumbs$.asObservable();
+//   }
+// }
+
+
+
+
+
+
+
+
+
